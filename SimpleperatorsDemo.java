49f87f9b577e951f794e5ac1794b2bd3ef0ac9e2
11249a050-pixel <11249a050@kanchiuniv.ac.in>
2026-07-22 11:57:44 +0530

    Add files via upload

---
+++ SimpleperatorsDemo.java
@@ -0,0 +1,14 @@
+public class SimpleperatorsDemo{
+public static void main(string[] args){
+int a=10,b=3;
+System.out.println("a+b=" + (a+b));
+System.out.println("a-b=" + (a-b));
+System.out.println("a>b?" + (a>b));
+booleaqn x=ture,y=flase;
+System.out.println("x&&y=" + (x&&y));
+a+=5;
+System.out.println("a after +=5:" +a);
+int max =(a>b)?a:b;
+System.out.println("maximum=" + max);
+}
+}
